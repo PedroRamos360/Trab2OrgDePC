2782fcd0a1e1abe72d7f7593c010465629ea5718
PedroRamos360 <ramosct70@hotmail.com>
2023-01-19 12:30:38 -0300

    correções quest2

--- quest2.asm
+++ quest2.asm
@@ -1,25 +1,31 @@
 .data
 x: .float 57.23
+index: .float 2
+resultado: .float 1
+incremento: .float 2
+decremento: .float 1
+criterio_de_parada: .float 1
+
 
 .text
 .macro power(%base, %exponent)
-    li.s $f7, %base
-    li.s $f8, %exponent
-    li.s $f9, 1
-    li.s $f14, -1
+    l.s $f7, %base
+    l.s $f8, %exponent
+    l.s $f9, resultado
+    l.s $f14, decremento
     loop:
         beq $f8, 0, endloop
         mul.s $f9, $f9, $f7
-        add.s $f8, $f8, $f14
+        sub.s $f8, $f8, $f14
         j loop
     endloop:
-    move $f30, $t2
+    mov.s $f30, $f9
 .end_macro
 
 .macro factorial(%number)
-    li.s $f10, 1           
-    li.s $f11, 1
-    li.s $f13, 1       
+    l.s $f10, resultado
+    l.s $f11, criterio_de_parada
+    l.s $f13, decremento
     loop:
         beq %number, $f11, endloop 
         mul.s $f10, $f10, $f11    
@@ -33,23 +39,22 @@ x: .float 57.23
 j main
 
 cosseno:
-    li.s $f2, 2 # index (baseado nos expoentes)
-    li.s $f3, 1 # valor inicial
-    li.s $f12, 2
-    move $f4, $f0 # $s2 = x
+    li $t0, 2 # index (baseado nos expoentes)
+    l.s $f3, resultado # resultado
+    mov.s $f4, $f0 # $f4 = $f0 = x
     for:
         bgt $f2, 14, for.end
-        power($f4, $f2)
-        move $f5, $f30 # $s3 = x^index
-        factorial($f2)
-        move $f6, $f30 # $s4 = fatorial(index)
+        power($f4, $t0)
+        mov.s $f5, $f30 # $s3 = x^index
+        factorial($t0)
+        mov.s $f6, $f30 # $s4 = fatorial(index)
         div.s $f5 $f5, $f6 # realiza a divisão e armazena em $f5
         sub.s $f3, $f3, $f5
         
-        add.s $f2, $f2, $f12
+        addi $t0, $t0, 2
     for.end:
     
-    move $f30, $s1
+    mov.s $f30, $f3
     
     
     jal $ra
@@ -58,7 +63,7 @@ cosseno:
 main:
     l.s $f0, x
     jal cosseno
-    move $f1, $f30
+    mov.s $f1, $f30
     li $v0, 2
     add $f12, $f1, $zero
     syscall
