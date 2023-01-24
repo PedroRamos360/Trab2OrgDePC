6e9c4f609320dd9571cbaabea8856dcdacf68f0e
PedroRamos360 <ramosct70@hotmail.com>
2023-01-23 21:08:35 -0300

    progresso

--- quest2.asm
+++ quest2.asm
@@ -1,63 +1,70 @@
 .data
-x: .float 57.23
+x: .float 1.0472
 index: .float 2
 resultado: .float 1
 incremento: .float 2
 decremento: .float 1
 criterio_de_parada: .float 1
+conversion: .word 0
+zero: .float 0
+expoente: .word 0
 
 
 .text
-.macro power(%base, %exponent)
-    l.s $f7, %base
-    l.s $f8, %exponent
+.macro power()
+    lw $t0, expoente
+    
     l.s $f9, resultado
-    l.s $f14, decremento
     loop:
-        beq $f8, 0, endloop
-        mul.s $f9, $f9, $f7
-        sub.s $f8, $f8, $f14
+        beq $t0, 0, endloop
+        mul.s $f9, $f9, $f0 # multiplica resultado pela base novamente
+        subi $t0, $t0, 1 # subtrai index do loop (expoente)
         j loop
     endloop:
     mov.s $f30, $f9
 .end_macro
 
-.macro factorial(%number)
+.macro factorial($number)
     l.s $f10, resultado
-    l.s $f11, criterio_de_parada
-    l.s $f13, decremento
+    li $t0, 1 # index para verificar parada do loop
     loop:
-        beq %number, $f11, endloop 
+        beq $number, $t0, endloop 
+    	mtc1 $t0, $f11
+    	cvt.s.w $f11, $f11
         mul.s $f10, $f10, $f11    
-        add.s $f11, $f11, $f13
+        addi $t0, $t0, 1
         j loop               
     endloop:
     
-    move $f30, $f10
+    mov.s $f30, $f10
 
 .end_macro
 j main
 
 cosseno:
-    li $t0, 2 # index (baseado nos expoentes)
+    li $s0, 2 # index (baseado nos expoentes)
     l.s $f3, resultado # resultado
     mov.s $f4, $f0 # $f4 = $f0 = x
     for:
-        bgt $f2, 14, for.end
-        power($f4, $t0)
+        bgt $s0, 14, for.end
+        cvt.w.s $f4, $f4 # converte float para int
+        s.s $f4, conversion # armazena num .word
+       	lw $t1, conversion # pega o float armazenado no .word no formato certo
+       	sw $s0, expoente
+        power # Recebe apenas o parametro expoente já que a base sempre será x para esse programa
         mov.s $f5, $f30 # $s3 = x^index
         factorial($t0)
         mov.s $f6, $f30 # $s4 = fatorial(index)
         div.s $f5 $f5, $f6 # realiza a divisão e armazena em $f5
         sub.s $f3, $f3, $f5
         
-        addi $t0, $t0, 2
+        addi $s0, $s0, 2
     for.end:
     
     mov.s $f30, $f3
     
     
-    jal $ra
+    jr $ra
     
 
 main:
@@ -65,5 +72,6 @@ main:
     jal cosseno
     mov.s $f1, $f30
     li $v0, 2
-    add $f12, $f1, $zero
+    l.s $f29, zero
+    add.s $f12, $f1, $f29
     syscall
